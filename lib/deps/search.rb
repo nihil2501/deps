d2dacb833d0e4d4b8646d59a3d012b685cf5fc76
Oren Mittman <nihil2501@gmail.com>
2020-04-30 13:29:40 -0400

    Search class instead of module

--- lib/deps/search.rb
+++ lib/deps/search.rb
@@ -3,77 +3,85 @@
 require "octokit"
 
 module Deps
-  module Search
-    GITHUB_ORG = "Leafly-com"
-    GITHUB_TOKEN = ENV["GITHUB_TOKEN"]
-    THIS_REPO = "deps"
-
+  class Search
     class << self
       def perform(q)
-        memo = []
-        return memo if q.blank?
+        new(q).perform
+      end
+    end
 
-        search(q).items.each do |item|
-          repo = item.repository
-          next if repo.name == THIS_REPO
+    def initialize(q)
+      @q = q
+    end
 
-          fragments = get_fragments(item, q)
-          next if fragments.empty?
+    def perform
+      memo = []
+      return memo if @q.blank?
 
-          repo = get_repo(repo)
-          next if repo.archived
+      search.items.each do |item|
+        repo = item.repository
+        next if repo.name == THIS_REPO
 
-          dep = Deps::Dep.new(repo.name, item.html_url)
-          dep.fragments = fragments
-          memo << dep
-        end
+        fragments = get_fragments(item)
+        next if fragments.empty?
 
-        memo
+        repo = get_repo(repo)
+        next if repo.archived
+
+        dep = Deps::Dep.new(repo.name, item.html_url)
+        dep.fragments = fragments
+        memo << dep
       end
 
-      private
-        def get_fragments(item, q)
-          [].tap do |memo|
-            item.text_matches.each do |match|
-              r = /\b#{Regexp.quote(q)}\b/
-              s = match.fragment
-
-              if s.match?(r)
-                memo << s
-              end
-            end
-          end
-        end
-
-        def search(q)
-          q = "#{q} org:#{GITHUB_ORG}"
-          opts = {
-            accept: "application/vnd.github.v3.text-match+json",
-            per_page: 100 # github's max
-          }
-
-          client.search_code(q, opts).tap do
-            rel = client.last_response.rels[:next]
-            if rel.present?
-              raise \
-                RuntimeError,
-                "Too many results"
-            end
-          end
-        end
-
-        def get_repo(repo)
-          client.repo(repo.id)
-        end
-
-        def client
-          unless defined? @client
-            @client = Octokit::Client.new(access_token: GITHUB_TOKEN)
-            # @client.middleware.response :logger
-          end
-
-          @client
-        end
+      memo
     end
+
+    private
+      GITHUB_ORG = "Leafly-com"
+      GITHUB_TOKEN = ENV["GITHUB_TOKEN"]
+      THIS_REPO = "deps"
+
+      def get_fragments(item)
+        [].tap do |memo|
+          item.text_matches.each do |match|
+            r = /\b#{Regexp.quote(@q)}\b/
+            s = match.fragment
+
+            if s.match?(r)
+              memo << s
+            end
+          end
+        end
+      end
+
+      def search
+        q = "#{@q} org:#{GITHUB_ORG}"
+        opts = {
+          accept: "application/vnd.github.v3.text-match+json",
+          per_page: 100 # github's max
+        }
+
+        client.search_code(q, opts).tap do
+          rel = client.last_response.rels[:next]
+          if rel.present?
+            raise \
+              RuntimeError,
+              "Too many results"
+          end
+        end
+      end
+
+      def get_repo(repo)
+        client.repo(repo.id)
+      end
+
+      def client
+        unless defined? @client
+          @client = Octokit::Client.new(access_token: GITHUB_TOKEN)
+          # @client.middleware.response :logger
+        end
+
+        @client
+      end
   end
 end
